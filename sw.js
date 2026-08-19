9895905bd9f0e202cf5fe5c940885b773bed25e2
pipadens <80138662+pipadens@users.noreply.github.com>
2026-08-19 12:11:58 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+// sw.js
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('hookups-store').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
